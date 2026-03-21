3283ad9ab4fd93e5072c914a508b40564abea932
Chapaio00 <zapallitove00@hotmail.com>
2026-03-21 17:50:44 -0300

    Hud de pc cambiado y botones implementados!

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ const aside = document.querySelector('.aside');
 btnImg.addEventListener('click', () => {
   aside.classList.toggle('show')
 })
+
+let  paginas = document.querySelectorAll(".pagina")
+let  paginasArray = Array.from(paginas) ;
+function avanzarPagina(){
+  let actual = paginasArray.findIndex(p => !p.classList.contains("paginaInvisible"));
+  if(paginasArray.length - 1 > actual){
+  paginasArray[actual].classList.remove("paginaVisible");
+  paginasArray[actual].classList.add("paginaInvisible");
+
+  let siguiente = (actual + 1);
+
+    paginasArray[siguiente].classList.remove("paginaInvisible");
+    paginasArray[siguiente].classList.add("paginaVisible");
+  }
+  }
+
+  function retrocederPagina(){
+    let actual = paginasArray.findIndex(p => !p.classList.contains("paginaInvisible"));
+    if(actual > 0){
+      paginasArray[actual].classList.remove("paginaVisible");
+      paginasArray[actual].classList.add("paginaInvisible");
+
+      let anterior =(actual - 1);
+      paginasArray[anterior].classList.remove("paginaInvisible");
+      paginasArray[anterior].classList.add("paginaVisible");
+
+    }
+  }
+
+
